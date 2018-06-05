3472dd3ba8f6b8e308b06c232e99c183899d9df9
Luis Gallardo <pinwy17.88@gmail.com>
2018-06-04 22:46:18 -0400

    ordenado del codigo 1

--- src/my-app.js
+++ src/my-app.js
@@ -32,147 +32,140 @@ setPassiveTouchGestures(true);
 setRootPath(MyAppGlobals.rootPath);
 
 class MyApp extends PolymerElement {
-  static get template() {
-    return html`
-      <style>
-        :host {
-          --app-primary-color: #4285f4;
-          --app-secondary-color: black;
+      static get template() {
+            return html`
+                  <style>
+                        :host {
+                              --app-primary-color: #4285f4;
+                              --app-secondary-color: black;
+                              display: block;
+                        }
+                        app-header {
+                              color: #fff;
+                              background-color: var(--app-primary-color);
+                        }
+                              app-header paper-icon-button {
+                                    --paper-icon-button-ink-color: white;
+                              }
 
-          display: block;
+                        app-drawer{
+                              color: red;
+                        }
+                              .drawer-list {
+                                    margin: 0 20px;
+                              }
+                                    .drawer-list a {
+                                          display: block;
+                                          padding: 0 16px;
+                                          text-decoration: none;
+                                          color: var(--app-secondary-color);
+                                          line-height: 40px;
+                                    }
+                                          .drawer-list a.iron-selected {
+                                                color: black;
+                                                font-weight: bold;
+                                          }
+                  </style>
+
+                  <app-location route="{{route}}" url-space-regex="^[[rootPath]]">
+                  </app-location>
+
+                  <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}">
+                  </app-route>
+
+                  <app-drawer-layout fullbleed="" narrow="{{narrow}}">
+                        <!-- Drawer content -->
+                        <app-drawer id="drawer" slot="drawer" opened="{{opened}}" swipe-open="[[narrow]]">
+                          <app-toolbar>Menu</app-toolbar>
+                          <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
+                            <a name="view1" href="[[rootPath]]view1">View One</a>
+                            <a name="view2" href="[[rootPath]]view2">View Two</a>
+                            <a name="view3" href="[[rootPath]]view3">View Three</a>
+                          </iron-selector>
+                        </app-drawer>
+
+                        <!-- Main content -->
+                        <app-header-layout has-scrolling-region="">
+                              <app-header slot="header" condenses="" reveals="" effects="waterfall">
+                                <app-toolbar>
+                                  <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
+                                  <div main-title="">My App</div>
+                                </app-toolbar>
+                              </app-header>
+
+                              <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
+                                <my-view1 name="view1"></my-view1>
+                                <my-view2 name="view2"></my-view2>
+                                <my-view3 name="view3"></my-view3>
+                                <my-view404 name="view404"></my-view404>
+                              </iron-pages>
+                        </app-header-layout>
+                  </app-drawer-layout>
+            `;
+      }
+
+      static get properties() {
+            return {
+                  page: {
+                        type: String,
+                        reflectToAttribute: true,
+                        observer: '_pageChanged'
+                  },
+                  opened: {
+                        type: Boolean,
+                        reflectToAttribute: true,
+                        observer: '_openedChanged'
+                  },
+                  routeData: Object,
+                  subroute: Object
+            };
+      }
+
+      static get observers() {
+            return [
+              '_routePageChanged(routeData.page)'
+            ];
+      }
+
+      _routePageChanged(page) {
+            // Muestra la página correspondiente según la ruta.
+            //
+            // Si no se encontró ninguna página en los datos de la ruta, la página será una cadena vacía.
+            // Muestra 'view1' en ese caso. Y si la página no existe, muestre 'view404'.
+            if (!page) {
+              this.page = 'view1';
+            } else if (['view1', 'view2', 'view3'].indexOf(page) !== -1) {
+              this.page = page;
+            } else {
+              this.page = 'view404';
+            }
+
+            // Cierre un cajón no persistente cuando se cambian la página y la ruta.
+            if (!this.$.drawer.persistent) {
+                  this.$.drawer.close();
+            }
+      }
+
+      _pageChanged(page) {
+        switch (page) {
+          case 'view1':
+            import('./my-view1.js');
+            break;
+          case 'view2':
+            import('./my-view2.js');
+            break;
+          case 'view3':
+            import('./my-view3.js');
+            break;
+          case 'view404':
+            import('./my-view404.js');
+            break;
         }
+      }
 
-       
+      _openedChanged(opened) {
+        console.log(opened);
+      }
 
-        app-header {
-          color: #fff;
-          background-color: var(--app-primary-color);
-        }
-
-        app-header paper-icon-button {
-          --paper-icon-button-ink-color: white;
-        }
-
-        .drawer-list {
-          margin: 0 20px;
-        }
-
-        .drawer-list a {
-          display: block;
-          padding: 0 16px;
-          text-decoration: none;
-          color: var(--app-secondary-color);
-          line-height: 40px;
-        }
-
-        .drawer-list a.iron-selected {
-          color: black;
-          font-weight: bold;
-        }
-      </style>
-
-      <app-location route="{{route}}" url-space-regex="^[[rootPath]]">
-      </app-location>
-
-      <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}">
-      </app-route>
-
-      <app-drawer-layout fullbleed="" narrow="{{narrow}}">
-        <!-- Drawer content -->
-        <app-drawer id="drawer" slot="drawer" opened="{{opened}}" swipe-open="[[narrow]]">
-          <app-toolbar>Menu</app-toolbar>
-          <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
-            <a name="view1" href="[[rootPath]]view1">View One</a>
-            <a name="view2" href="[[rootPath]]view2">View Two</a>
-            <a name="view3" href="[[rootPath]]view3">View Three</a>
-          </iron-selector>
-        </app-drawer>
-
-        <!-- Main content -->
-        <app-header-layout has-scrolling-region="">
-
-          <app-header slot="header" condenses="" reveals="" effects="waterfall">
-            <app-toolbar>
-              <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
-              <div main-title="">My App</div>
-            </app-toolbar>
-          </app-header>
-
-          <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
-            <my-view1 name="view1"></my-view1>
-            <my-view2 name="view2"></my-view2>
-            <my-view3 name="view3"></my-view3>
-            <my-view404 name="view404"></my-view404>
-          </iron-pages>
-        </app-header-layout>
-      </app-drawer-layout>
-    `;
-  }
-
-  static get properties() {
-    return {
-      page: {
-        type: String,
-        reflectToAttribute: true,
-        observer: '_pageChanged'
-      },
-      opened: {
-        type: Boolean,
-        reflectToAttribute: true,
-        observer: '_openedChanged'
-      },
-      routeData: Object,
-      subroute: Object
-    };
-  }
-
-  static get observers() {
-    return [
-      '_routePageChanged(routeData.page)'
-    ];
-  }
-
-  _routePageChanged(page) {
-     // Muestra la página correspondiente según la ruta.
-    //
-    // Si no se encontró ninguna página en los datos de la ruta, la página será una cadena vacía.
-    // Muestra 'view1' en ese caso. Y si la página no existe, muestre 'view404'.
-    if (!page) {
-      this.page = 'view1';
-    } else if (['view1', 'view2', 'view3'].indexOf(page) !== -1) {
-      this.page = page;
-    } else {
-      this.page = 'view404';
-    }
-
-    // Cierre un cajón no persistente cuando se cambian la página y la ruta.
-    if (!this.$.drawer.persistent) {
-      this.$.drawer.close();
-    }
-  }
-  _openedChanged(opened){
-    console.log(opened);
- }
-  _pageChanged(page) {
-     // Importar el componente de la página bajo demanda.
-     //
-     // Nota: `polymer build` no le gusta la concatenación de cadenas en la importación
-     // declaración, así que desglosa.
-    switch (page) {
-      case 'view1':
-        import('./my-view1.js');
-        break;
-      case 'view2':
-        import('./my-view2.js');
-        break;
-      case 'view3':
-        import('./my-view3.js');
-        break;
-      case 'view404':
-        import('./my-view404.js');
-        break;
-    }
-  }
 }
-
 window.customElements.define('my-app', MyApp);
